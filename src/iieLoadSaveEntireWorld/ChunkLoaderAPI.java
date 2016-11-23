4dd10489948da62110596003cda04e0e2fcb1678
alisolarflare <alisunderstorm@gmail.com>
2016-11-22 22:54:36 -0500

    Created methods that load and unload chunks

--- src/iieLoadSaveEntireWorld/ChunkLoaderAPI.java
+++ src/iieLoadSaveEntireWorld/ChunkLoaderAPI.java
@@ -1,5 +1,31 @@
 package iieLoadSaveEntireWorld;
 
+import org.bukkit.Location;
+
 public class ChunkLoaderAPI {
+	/**This method loads a chunk if the chunk isn't loaded already.
+	 * @param locationToLoad
+	 * @return True if chunk was already loaded, False if chunk wasn't already loaded
+	 */
+	public static boolean loadChunk(Location locationToLoad){
+		if(!(locationToLoad.getBlock().getChunk().isLoaded())){
+			locationToLoad.getBlock().getChunk().load();
+			return true;
+		}
+		return false;
+	}
+	
+
+	/**This method loads a chunk if the chunk isn't loaded already.
+	 * @param locationToLoad
+	 * @return True if chunk was already unloaded, False if chunk wasn't already unloaded
+	 */
+	public static boolean unloadChunk(Location locationToUnload){
+		if (locationToUnload.getBlock().getChunk().isLoaded()){
+			locationToUnload.getBlock().getChunk().unload();
+			return false;
+		}
+		return true;
+	}
 
 }
